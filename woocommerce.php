fcac74177793dd4549b06027e0cfcd2ad202b1aa
Carley Kuehner <ckuehner@cosmicktechnologies.com>
2025-06-08 23:34:15 -0400

    woocommerce width

--- woocommerce.php
+++ woocommerce.php
@@ -12,18 +12,27 @@ get_header();
 get_template_part( 'template-parts/page', 'header' );
 ?>
 
-<?php if ( is_product_category() && is_active_sidebar( 'shop-sidebar' ) ) : ?>
-    <div class="col-lg-9">
-<?php else : ?>
-    <div class="col-12">
-<?php endif; ?>
-        <?php woocommerce_content(); ?>
-    </div>
-<?php if ( is_product_category() && is_active_sidebar( 'shop-sidebar' ) ) : ?>
-    <div class="col-lg-3">
-        <?php get_sidebar( 'shop' ); ?>
-    </div>
-<?php endif; ?>
+    <main id="primary" class="site-main py-5">
+        <div class="main-container">
+            <div class="row">
+                <?php
+                // Check if it is a product archive page (like category or shop) and if the sidebar is active.
+                if ( is_archive() && is_active_sidebar( 'shop-sidebar' ) ) :
+                    ?>
+                    <div class="col-lg-9">
+                        <?php woocommerce_content(); ?>
+                    </div>
+                    <div class="col-lg-3">
+                        <?php get_sidebar( 'shop' ); ?>
+                    </div>
+                <?php else : ?>
+                    <div class="col-12">
+                        <?php woocommerce_content(); ?>
+                    </div>
+                <?php endif; ?>
+            </div>
+        </div>
+    </main>
 
 <?php
 get_footer();
